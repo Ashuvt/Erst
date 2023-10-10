1829d5d1168c8665d062dc3222ee7689a78882bd
Ashish <thummarav@gmail.com>
2023-10-10 16:30:17 +0530

    Initial SetUp with SASS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+    <div className='app'>
+        <h1> Heading 1</h1>
+        <h2> fdsa jdsaj fsdk fjdlks fsjlkd</h2>
+        <h3> fdsa jdsaj fsdk fjdlks fsjlkd</h3>
+        <h4> fdsa jdsaj fsdk fjdlks fsjlkd</h4>
+        <h5> fdsa jdsaj fsdk fjdlks fsjlkd</h5>
+        <h6> fdsa jdsaj fsdk fjdlks fsjlkd</h6>
+<button className="btn btn-danger">sdfdsf</button>
+    </div>
     </div>
   );
 }
