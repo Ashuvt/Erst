16277b6d27b62b36d3d0f8d21a0fdfebff32a801
Ashish <thummarav@gmail.com>
2023-10-23 20:47:30 +0530

    Add Icons from figma

---
+++ src/utils/images/icons.js
@@ -0,0 +1,90 @@
+import angleUp from "../../assets/images/icons/angleup.svg";
+import angleRight from "../../assets/images/icons/angleright.svg";
+import angleDown from "../../assets/images/icons/angledown.svg";
+import angleLeft from "../../assets/images/icons/angleleft.svg";
+import arrowUp from "../../assets/images/icons/arrowup.svg";
+import arrowRight from "../../assets/images/icons/arrowright.svg";
+import arrowDown from "../../assets/images/icons/arrowdown.svg";
+import arrowLeft from "../../assets/images/icons/arrowleft.svg";
+import unselectedRadio from "../../assets/images/icons/radiobtn.svg";
+import selectedRadio from "../../assets/images/icons/btnselected.svg";
+import eye from "../../assets/images/icons/eye.svg";
+import eyeSlash from "../../assets/images/icons/slasheye.svg";
+import search from "../../assets/images/icons/search.svg";
+import user from "../../assets/images/icons/user.svg";
+import star from "../../assets/images/icons/star.svg";
+import clock from "../../assets/images/icons/clock.svg";
+import home from "../../assets/images/icons/home.svg";
+import group from "../../assets/images/icons/group.svg";
+import save from "../../assets/images/icons/save.svg";
+import saved from "../../assets/images/icons/saved.svg";
+import live from "../../assets/images/icons/live.svg";
+import email from "../../assets/images/icons/email.svg";
+import students from "../../assets/images/icons/student.svg";
+import skillpath from "../../assets/images/icons/skilpath.svg";
+import notification from "../../assets/images/icons/notification.svg";
+import calender from "../../assets/images/icons/calender.svg";
+import explore from "../../assets/images/icons/explore.svg";
+import module from "../../assets/images/icons/modules.svg";
+import courses from "../../assets/images/icons/courses.svg";
+import quizandproject from "../../assets/images/icons/quizandproject.svg";
+import question from "../../assets/images/icons/question.svg";
+import upload from "../../assets/images/icons/upload.svg";
+import reviewed from "../../assets/images/icons/reviewed.svg";
+import logout from "../../assets/images/icons/logout.svg";
+import profile from "../../assets/images/icons/profile.svg";
+import help from "../../assets/images/icons/help.svg";
+import playfill from "../../assets/images/icons/playfill.svg";
+
+
+
+
+
+
+export const icons = {
+  angleUp,
+  angleRight,
+  angleDown,
+  angleLeft,
+  arrowUp,
+  arrowRight,
+  arrowDown,
+  arrowLeft,
+  unselectedRadio,
+  selectedRadio,
+  eye,
+  eyeSlash,
+  search,
+  user,
+  star,
+  clock,
+  home,
+  group,
+  save,
+  saved,
+  live,
+  email,
+  students,
+  skillpath,
+  notification,
+  calender,
+  explore,
+  module,
+  courses,
+  certificate,
+  quizandproject,
+  question,
+  upload,
+  reviewed,
+  logout,
+  profile,
+  help,
+
+
+
+
+
+
+  playfill
+
+};
