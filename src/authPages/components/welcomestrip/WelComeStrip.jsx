bee5837119221c0f762c61d1503144f29f7eb72a
Ashish <thummarav@gmail.com>
2023-10-31 11:31:48 +0530

    adding animation

--- src/authPages/components/welcomestrip/WelComeStrip.jsx
+++ src/authPages/components/welcomestrip/WelComeStrip.jsx
@@ -1,23 +1,58 @@
 import { resetAllToggler } from "../../../store/actions";
-import { icon } from "../../../utils/images/icons";
+import { useEffect, useState } from "react";
 import { images } from "../../../utils/images/images";
 import "./WelComeStrip.scss";
 import { useDispatch } from "react-redux";
+import WOW from "wow.js";
 
-const WelComeStrip = ({title, text}) => {
+const WelComeStrip = ({ title, text }) => {
+  const [titleText, setTitleText] = useState("");
+
+  useEffect(() => {
+    const wow = new WOW();
+    wow.init();
+
+    let currentIndex = 1;
+    let direction = true;
+
+    const typingTimer = setInterval(() => {
+
+      if(currentIndex == title.length + 1){
+        direction = false;
+      }
+      if(currentIndex == 1){
+        direction = true;
+      }
+
+      if (currentIndex <= title.length && direction) {
+        setTitleText(title.slice(0, currentIndex));
+        currentIndex++;
+      } else {
+        setTitleText(title.slice(0, currentIndex));
+        currentIndex--;
+      }
+
+    }, 200); // Adjust the typing speed (in milliseconds) here
+    return () => clearInterval(typingTimer);
+
+  }, []);
+
+
+
+
+
 
   const dispatch = useDispatch();
 
   const resetToggler = () => {
-    dispatch({type:resetAllToggler()});
-  }
-
+    dispatch({ type: resetAllToggler() });
+  };
 
   return (
     <section className="welcome_strip" onClick={resetToggler}>
       <div className="screen_container">
-        <h1>{title}</h1>
-        <p>{text}</p>
+        <h1 className="wow slideInUp">{titleText}</h1>
+        <p className="wow slideInUp">{text}</p>
       </div>
       <img src={images.bgPattern} alt="pattern" className="left" />
       <img src={images.bgPattern} alt="pattern" className="right" />
